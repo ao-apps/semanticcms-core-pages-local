066352b800c972d19eb4341086e3a2e6ea769752
AO Industries, Inc. <support@aoindustries.com>
2017-08-23 20:46:44 -0500

    PageContext moved to new semanticcms-core-pages-local module

---
+++ src/main/java/com/semanticcms/core/pages/local/PageContextCallable.java
@@ -0,0 +1,43 @@
+/*
+ * semanticcms-core-pages-local - Support for SemanticCMS pages produced by the local servlet container.
+ * Copyright (C) 2016, 2017  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-core-pages-local.
+ *
+ * semanticcms-core-pages-local is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-core-pages-local is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-core-pages-local.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.core.pages.local;
+
+import com.aoindustries.util.concurrent.ThreadLocalsCallable;
+import java.util.concurrent.Callable;
+
+/**
+ * Maintains current page context for the provided callable.
+ */
+public class PageContextCallable<T> extends ThreadLocalsCallable<T> {
+
+	static final ThreadLocal<?>[] threadLocals = {
+		PageContext.servletContext,
+		PageContext.request,
+		PageContext.response,
+		PageContext.out
+	};
+
+	public PageContextCallable(Callable<T> task) {
+		super(task, threadLocals);
+	}
+}
