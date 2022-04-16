7fdebbc94cbedc9a64c95f8b6a1ee648aca967b2
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:14 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/core/pages/local/CurrentPage.java
+++ src/main/java/com/semanticcms/core/pages/local/CurrentPage.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-pages-local.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.pages.local;
 
 import com.aoapps.servlet.attribute.ScopeEE;
--- src/main/java/com/semanticcms/core/pages/local/PageContextCallable.java
+++ src/main/java/com/semanticcms/core/pages/local/PageContextCallable.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-pages-local - Support for SemanticCMS pages produced by the local servlet container.
- * Copyright (C) 2016, 2017, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-pages-local.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.pages.local;
 
 import com.aoapps.lang.concurrent.ThreadLocalsCallable;
